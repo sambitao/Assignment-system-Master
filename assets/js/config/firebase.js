6387d3018025e4642bfc0d14e04c235d848148ad
sambitao <37581424+sambitao@users.noreply.github.com>
2026-02-19 15:13:46 +0700

    Add files via upload

--- assets/js/config/firebase.js
+++ assets/js/config/firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZVvsZcsdrCDYb5JqiZFLaN52hjUi_Arw",
+  authDomain: "assignment-interruption-e9481.firebaseapp.com",
+  projectId: "assignment-interruption-e9481",
+  storageBucket: "assignment-interruption-e9481.firebasestorage.app",
+  messagingSenderId: "246855027730",
+  appId: "1:246855027730:web:a61bd1dcb9e103c3cc9ccb"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
